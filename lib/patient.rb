0605a7eacda367aef0800866dc28f69e91123d5a
Laura Radville <laura.radville@gmail.com>
2019-07-29 20:35:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,8 +14,11 @@ class Patient
   end
   
   def new_appointment(doctor, date)
-    Appointment.new
+    Appointment.new(date, self, doctor)
   end
   
+  def appointments
+    Appointment.all.map { 
+  end
   
 end
