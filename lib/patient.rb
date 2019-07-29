7c25a42171c52553f53778112cf246faf115b3c5
Laura Radville <laura.radville@gmail.com>
2019-07-29 20:34:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,14 +1,20 @@
 class Patient
   
-  
+  attr_reader :name
   
   @@all = []
   
-  def initialize
+  def initialize(name)
+    @name = name
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new
   end
   
   
